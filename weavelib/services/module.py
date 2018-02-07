72f97de583cf22b183c9d4711024754dd7363dbf
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-02-06 19:07:24 -0800

    Adding package_path for module objects.
    This helps as a key to retrieve App Info in RPCs for instance.

--- weavelib/services/module.py
+++ weavelib/services/module.py
@@ -1,8 +1,9 @@
 class Module(object):
-    def __init__(self, name, deps, meta):
+    def __init__(self, name, deps, meta, package_path):
         self.name = name
         self.deps = deps
         self.meta = meta
+        self.package_path = package_path
 
     def get_config(self):
         return self.meta.get("config", [])
