a307a1d0d751f8da1f2af4d703a53c11ca1de334
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-03-22 22:46:10 -0700

    AppHTTPServer.

--- weavelib/http/apphttp.py
+++ weavelib/http/apphttp.py
@@ -1,10 +1,22 @@
+import json
+import os
+import sys
+
+
+def load_ui(path, service):
+    module = sys.modules[service.__class__.__module__]
+    service_dir = os.path.dirname(module.__file__)
+    with open(os.path.join(service_dir, path)) as inp:
+        return json.load(inp)
+
+
 class AppHTTPServer(object):
-    def __init__(self, service, root_obj):
+    def __init__(self, service):
         self.service = service
-        self.root_obj = root_obj
 
     def start(self):
         pass
 
-    def add_url(self, obj, block=True):
-        return self.service.rpc_client["register_app_view"](obj, _block=block)
+    def add_url(self, path, block=True):
+        obj = load_ui(path, self.service)
+        return self.service.rpc_client["register_view"](obj, _block=block)
