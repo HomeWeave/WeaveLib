12fa731eb9ee96a098c44e06aae96da7c984fdc5
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-05-05 22:02:53 -0700

    Move all exceptions to a central place.

---
+++ weavelib/exceptions.py
@@ -0,0 +1,42 @@
+class WeaveException(Exception):
+    def __init__(self, extra=None):
+        self.extra = extra
+
+    def err_msg(self):
+        return self.__class__.__name__
+
+
+class ObjectNotFound(WeaveException):
+    pass
+
+
+class ObjectAlreadyExists(WeaveException):
+    pass
+
+
+class ObjectClosed(WeaveException):
+    pass
+
+
+class BadOperation(WeaveException):
+    pass
+
+
+class BadArguments(WeaveException):
+    pass
+
+
+class InternalError(WeaveException):
+    pass
+
+
+class ProtocolError(WeaveException):
+    pass
+
+
+class AuthenticationFailed(WeaveException):
+    pass
+
+
+class SchemaValidationFailed(WeaveException):
+    pass
