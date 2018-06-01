ca4d009228e243e1bdc36e21c927352328bebe0e
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-06-01 00:10:56 -0700

    Add file watcher to listen for changes and update HTTP resources during development.

---
+++ tests/http/test_app_http.py
@@ -0,0 +1,81 @@
+import time
+
+import requests
+from weaveserver.core.services import ServiceManager
+
+from weavelib.services import BaseService
+from weavelib.http import AppHTTPServer
+from weavelib.http.apphttp import path_from_service
+
+
+AUTH = {
+    "auth1": {
+        "type": "SYSTEM",
+        "appid": "appmgr"
+    },
+    "auth2": {
+        "appid": "appid2",
+        "package": "p"
+    }
+}
+
+
+class DummyService(BaseService):
+    def __init__(self, token):
+        super(DummyService, self).__init__(token)
+        self.http = AppHTTPServer(self)
+
+    def on_service_start(self):
+        super(DummyService, self).on_service_start()
+        self.http.start()
+
+    def on_service_stop(self):
+        self.http.stop()
+        super(DummyService, self).on_service_stop()
+
+
+class TestAppHTTPServer(object):
+    @classmethod
+    def setup_class(cls):
+        cls.service_manager = ServiceManager()
+        cls.service_manager.apps.update(AUTH)
+        cls.service_manager.start_services(["messaging", "appmanager"])
+
+        cls.service = DummyService("auth2")
+        cls.service.service_start()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.service.service_stop()
+        cls.service_manager.stop()
+
+    def test_register_folder(self):
+        base_url = self.service.http.register_folder("static")
+        assert base_url == "/apps/appid2"
+
+        url = "http://localhost:5000" + base_url
+        assert requests.get(url + "/_status-card.json").json() == {}
+
+        assert requests.get(url + "/test.txt").text == "test\n"
+
+        assert requests.get(url + "/folder/test.md").text == "hello world\n"
+
+    def test_folder_watcher(self):
+        path = path_from_service("static2/temp.txt", self.service)
+
+        base_url = self.service.http.register_folder("static2", watch=True)
+        assert base_url == "/apps/appid2"
+
+        url = "http://localhost:5000" + base_url
+        assert requests.get(url + "/temp.txt").text == "temp\n"
+
+        path = path_from_service("static2/temp.txt", self.service)
+        with open(path, "w") as f:
+            f.write("new-value")
+
+        time.sleep(5)
+
+        assert requests.get(url + "/temp.txt").text == "new-value"
+
+        with open(path, "w") as f:
+            f.write("temp\n")
--- weavelib/http/apphttp.py
+++ weavelib/http/apphttp.py
@@ -3,6 +3,9 @@ import inspect
 import mimetypes
 import os
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, FileModifiedEvent
+
 
 mimetypes.init()
 
@@ -21,38 +24,74 @@ def encode_content(path, service):
         return encoded.decode('ascii')
 
 
+def register_url(service, cur_file, path, mime=None, block=True):
+    rel_path = os.path.relpath(cur_file, path)
+    obj = encode_content(cur_file, service)
+
+    mime = mime or mimetypes.guess_type(cur_file)[0]
+    if not mime:
+        raise ValueError("Can't guess mime type. Please specify.")
+
+    url = service.rpc_client["register_view"](rel_path, obj, mime, _block=block)
+
+    if url and url.endswith(rel_path):
+        return url[:-len(rel_path)], rel_path
+
+
+class FileSystemUpdater(FileSystemEventHandler):
+    def __init__(self, callback):
+        self.callback = callback
+
+    def on_modified(self, event):
+        if isinstance(event, FileModifiedEvent):
+            self.callback(event.src_path)
+
+
+class FileWatcher(Observer):
+    def __init__(self, base_path, service):
+        super(FileWatcher, self).__init__()
+        self.base_path = base_path
+
+        def callback(path):
+            register_url(service, path, base_path)
+
+        self.updater = FileSystemUpdater(callback)
+
+    def start(self):
+        self.schedule(self.updater, path=self.base_path, recursive=True)
+        super(FileWatcher, self).start()
+
+    def stop(self):
+        super(FileWatcher, self).stop()
+        super(FileWatcher, self).join()
+
+
 class AppHTTPServer(object):
     def __init__(self, service):
         self.service = service
+        self.watchers = []
 
     def start(self):
         pass
 
-    def add_url(self, url, path, mime=None, block=True):
-        obj = encode_content(path, self.service)
+    def stop(self):
+        for watcher in self.watchers:
+            watcher.stop()
+        for watcher in self.watchers:
+            watcher.join()
 
-        mime = mime or mimetypes.guess_type(path)[0]
-        if not mime:
-            raise ValueError("Can't guess mime type. Please specify.")
-
-        return self.service.rpc_client["register_view"](url, obj, mime,
-                                                        _block=block)
-
-    def register_folder(self, path):
-        VIEW_MIME = "application/vnd.weaveview+json"
-
+    def register_folder(self, path, watch=False):
         path = path_from_service(path, self.service)
         base_url = None
         for cur_folder, _, files in os.walk(path):
             for filename in files:
                 cur_file = os.path.join(cur_folder, filename)
-                url = os.path.relpath(cur_file, path)
-                if url == "index.json":
-                    http_url = self.add_url(url, cur_file, mime=VIEW_MIME)
-                else:
-                    http_url = self.add_url(url, cur_file)
+                prefix_url, rel_url = register_url(self.service, cur_file, path)
 
-                if base_url is None and http_url and http_url.endswith(url):
-                    base_url = http_url[:-len(url)]
+                if base_url is None:
+                    base_url = prefix_url
+        if watch:
+            self.watchers.append(FileWatcher(path, self.service))
+            self.watchers[-1].start()
 
         return base_url.rstrip("/")
