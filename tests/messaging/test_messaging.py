3ce386375e2700e9f8a9575f9a78d452ee9ab541
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-09-23 18:03:32 -0700

    Fix tests.

--- tests/messaging/test_messaging.py
+++ tests/messaging/test_messaging.py
@@ -4,8 +4,8 @@ import pytest
 
 import weavelib.netutils as netutils
 from weavelib.exceptions import AuthenticationFailed, ProtocolError
-from weavelib.messaging import discover_message_server, Sender
-from weavelib.messaging import Receiver, Creator
+from weavelib.messaging import discover_message_server
+from weavelib.messaging import WeaveConnection, Creator
 
 from weaveserver.services.discovery import DiscoveryService
 from weaveserver.services.discovery.service import DiscoveryServer
@@ -85,18 +85,23 @@ class TestCreator(object):
         cls.service_manager.apps.update(AUTH)
         cls.service_manager.start_services(["core"])
 
+        cls.conn = WeaveConnection.local()
+        cls.conn.connect()
+
     @classmethod
     def teardown_class(cls):
+        cls.conn.close()
         cls.service_manager.stop()
+        import faulthandler; faulthandler.dump_traceback()
 
     def test_create_without_auth(self):
-        creator = Creator()
+        creator = Creator(self.conn)
         creator.start()
         with pytest.raises(ProtocolError):
             creator.create({"queue_name": "/test"})
 
     def test_create_bad_auth(self):
-        creator = Creator(auth="bad-auth")
+        creator = Creator(self.conn, auth="bad-auth")
         creator.start()
         with pytest.raises(AuthenticationFailed):
             creator.create({"queue_name": "/test"})
