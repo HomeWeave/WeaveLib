d0e6e67c313939cbce1849b5dc368b6842af5f6d
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-09-23 22:40:29 -0700

    Fix tests.

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,8 +4,9 @@ from tempfile import NamedTemporaryFile
 import pytest
 from weaveserver.core.services import ServiceManager
 
-from weavelib.services import BaseService
 from weavelib.db import AppDBConnection
+from weavelib.messaging import WeaveConnection
+from weavelib.services import BaseService
 from weaveserver.core.logger import configure_logging
 
 configure_logging()
@@ -20,9 +21,9 @@ AUTH = {
 
 
 class DummyService(BaseService):
-    def __init__(self, token):
+    def __init__(self, conn, token):
         super(DummyService, self).__init__(token)
-        self.db = AppDBConnection(self)
+        self.db = AppDBConnection(conn, self)
 
     def on_service_start(self):
         super(DummyService, self).on_service_start()
@@ -41,12 +42,16 @@ class TestAppDBConnection(object):
         cls.service_manager.apps.update(AUTH)
         cls.service_manager.start_services(["core", "simpledb"])
 
-        cls.service = DummyService("auth1")
+        cls.conn = WeaveConnection.local()
+        cls.conn.connect()
+
+        cls.service = DummyService(cls.conn, "auth1")
         cls.service.service_start()
 
     @classmethod
     def teardown_class(cls):
         cls.service.service_stop()
+        cls.conn.close()
 
         cls.service_manager.stop()
         os.unlink(os.environ["DB_PATH"])
