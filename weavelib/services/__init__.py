a8c8c48f3dae9d826833d78d24e0400dc1f8849e
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2019-04-03 23:26:47 -0700

    Fix imports.

--- weavelib/services/__init__.py
+++ weavelib/services/__init__.py
@@ -1,15 +1,11 @@
 from .module import Module
 from .service_base import BaseService
-from .service_base import BackgroundThreadServiceStart
-from .service_base import BackgroundProcessServiceStart
 from .service_base import BasePlugin
-from .service_base import RemotePlugin
+from .service_base import MessagingEnabled
 
 __all__ = [
     'BaseService',
-    'BackgroundThreadServiceStart',
-    'BackgroundProcessServiceStart',
     'BasePlugin',
-    'RemotePlugin',
+    'MessagingEnabled',
     'Module'
 ]
